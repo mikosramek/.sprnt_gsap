1e4975006c6f650cd65b8f9c031af6dcee725ed2
mikosramek <miko@mikosramek.ca>
2020-01-17 20:49:34 -0500

    Adding the about section animations

--- scripts/script.js
+++ scripts/script.js
@@ -18,6 +18,7 @@ animations.getRandomNumber = (min, max) => {
   return min + Math.floor(Math.random() * max);
 }
 
+
 $(document).ready(function(){
   gsap.from("h1", animations.slideIn);
 
@@ -27,4 +28,24 @@ $(document).ready(function(){
     gsap.to(this, animations.normal);
   });
 
+  var aboutIntro = gsap.timeline({paused: true});
+  aboutIntro
+    .addLabel("text")
+    .to('#quoteOne', {duration: 1.4, rotation: -5}, "text")
+    .to('#quoteTwo', {duration: 1.4, rotation: 5}, "text")
+    .addLabel("stars")
+    .from('#star1', {duration: 0.8, x: -150, y: -100, opacity: 0}, "stars")
+    .from('#star2', {duration: 1.2, x: -150, y: 200, opacity: 0}, "stars")
+    .from('#star3', {duration: 0.6, x: 200, y: -150, opacity: 0}, "stars")
+    .from('#star4', {duration: 1, x: 200, y: 150, opacity: 0}, "stars");
+  $(window).scroll( function(){
+    var currentScroll = $(this).scrollTop();
+    var elementHeight = $('#about').height();
+      if( currentScroll > elementHeight - 50 && currentScroll > 0 ){
+        windowScroll = currentScroll/elementHeight;
+        aboutIntro.play();
+      }
+  });
+
 });
+
